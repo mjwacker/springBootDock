9e137e62163c236cb12c2ef5c33f77ee99deba12
Miles <mileswacker@yahoo.com>
2021-10-09 09:11:28 -0600

    Initial commit

---
+++ src/test/java/wac/miles/springBootTest/SpringBootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package wac.miles.springBootTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
